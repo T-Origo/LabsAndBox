1576c92a5f9472eb0542e947435d0832960f710a
Salem Akowe <salemakowe@gmail.com>
2022-09-08 19:17:30 +0100

    sign in and sign up page

--- labs/lib/signin.dart
+++ labs/lib/signin.dart
@@ -28,6 +28,20 @@ class _SignInState extends State<SignIn> {
               ),
             ),
           ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: const [
+              Text(
+                "Labs N Box",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontFamily: "NotoSans",
+                  fontSize: 30,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ],
+          ),
           Positioned(
             child: Center(
               child: Container(
@@ -95,11 +109,109 @@ class _SignInState extends State<SignIn> {
                         )
                       ],
                     ),
+                    const SizedBox(
+                      height: 30,
+                    ),
+                    Form(
+                      child: Column(
+                        children: [
+                          TextFormField(
+                            decoration: const InputDecoration(
+                              hintText: "Registered Email ID/ Phone Number",
+                              hintStyle: TextStyle(
+                                fontFamily: "NotoSans",
+                                color: Kolors.textColor,
+                              ),
+                              enabledBorder: OutlineInputBorder(
+                                borderRadius: BorderRadius.all(
+                                  Radius.circular(10),
+                                ),
+                                borderSide: BorderSide(
+                                  color: Colors.grey,
+                                ),
+                              ),
+                              focusedBorder: OutlineInputBorder(
+                                borderRadius: BorderRadius.all(
+                                  Radius.circular(10),
+                                ),
+                                borderSide: BorderSide(
+                                  color: Colors.grey,
+                                ),
+                              ),
+                            ),
+                          ),
+                          const SizedBox(
+                            height: 30,
+                          ),
+                          TextFormField(
+                            decoration: const InputDecoration(
+                              hintText: "Enter your password",
+                              hintStyle: TextStyle(
+                                fontFamily: "NotoSans",
+                                color: Kolors.textColor,
+                              ),
+                              enabledBorder: OutlineInputBorder(
+                                borderRadius: BorderRadius.all(
+                                  Radius.circular(10),
+                                ),
+                                borderSide: BorderSide(
+                                  color: Colors.grey,
+                                ),
+                              ),
+                              focusedBorder: OutlineInputBorder(
+                                borderRadius: BorderRadius.all(
+                                  Radius.circular(10),
+                                ),
+                                borderSide: BorderSide(
+                                  color: Colors.grey,
+                                ),
+                              ),
+                            ),
+                          ),
+                          const SizedBox(
+                            height: 30,
+                          ),
+                          ElevatedButton(
+                            style: ElevatedButton.styleFrom(
+                              backgroundColor: Kolors.buttonColor,
+                            ),
+                            onPressed: () {},
+                            child: const Text(
+                              "Sign In",
+                              style: TextStyle(
+                                fontFamily: "NotoSans",
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
                   ],
                 ),
               ),
             ),
           ),
+          Positioned(
+            child: Align(
+              alignment: Alignment.bottomCenter,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text("Made in"),
+                      Icon(
+                        Icons.heart_broken,
+                        color: Colors.white,
+                      ),
+                      Text("with Science")
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          )
         ],
       ),
     );
