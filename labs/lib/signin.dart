48f0710dd1bb321c295dcde76753ce1c196f2d66
Salem Akowe <salemakowe@gmail.com>
2022-09-08 21:25:47 +0100

    still having overflow error

--- labs/lib/signin.dart
+++ labs/lib/signin.dart
@@ -15,141 +15,160 @@ class _SignInState extends State<SignIn> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Kolors.backgroundColor.withOpacity(.85),
-      body: Stack(
+      //backgroundColor: Kolors.backgroundColor.withOpacity(.85),
+      body: ListView(
         children: [
-          Positioned(
-            child: Container(
-              decoration: const BoxDecoration(
-                image: DecorationImage(
-                  image: AssetImage("img/bg.png"),
-                  fit: BoxFit.cover,
+          Column(
+            children: [
+              Container(
+                height: MediaQuery.of(context).size.height,
+                width: MediaQuery.of(context).size.width,
+                decoration: const BoxDecoration(
+                  color: Kolors.backgroundColor,
+                  image: DecorationImage(
+                    image: AssetImage("img/bg.png"),
+                    fit: BoxFit.cover,
+                  ),
                 ),
-              ),
-            ),
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: const [
-              Text(
-                "Labs N Box",
-                style: TextStyle(
-                  color: Colors.white,
-                  fontFamily: "NotoSans",
-                  fontSize: 30,
-                  fontWeight: FontWeight.bold,
+                child: Column(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(top: 17),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: const [
+                          Text(
+                            "Labs N Box",
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontFamily: "NotoSans",
+                              fontSize: 30,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+
+                    //Place for the input forms
+
+                    Positioned(
+                      child: Center(
+                        child: Container(
+                          height: 380,
+                          padding: const EdgeInsets.all(15),
+                          width: MediaQuery.of(context).size.width - 40,
+                          margin: const EdgeInsets.symmetric(horizontal: 20),
+                          decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(15),
+                            boxShadow: [
+                              BoxShadow(
+                                color: Colors.black.withOpacity(0.3),
+                                blurRadius: 15,
+                                spreadRadius: 5,
+                              ),
+                            ],
+                          ),
+                          child: Column(
+                            children: [
+                              Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceAround,
+                                children: [
+                                  GestureDetector(
+                                    onTap: () {
+                                      setState(() {
+                                        isSignIn = true;
+                                      });
+                                    },
+                                    child: Column(
+                                      children: [
+                                        Text(
+                                          "Sign In",
+                                          style: TextStyle(
+                                            color: isSignIn
+                                                ? Kolors.buttonColor
+                                                : Kolors.textColor,
+                                            fontWeight: FontWeight.bold,
+                                            fontSize: 23,
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                  ),
+                                  GestureDetector(
+                                    onTap: () {
+                                      setState(() {
+                                        isSignIn = false;
+                                      });
+                                    },
+                                    child: Column(
+                                      children: [
+                                        Text(
+                                          "Sign up",
+                                          style: TextStyle(
+                                            color: isSignIn
+                                                ? Kolors.textColor
+                                                : Kolors.buttonColor,
+                                            fontWeight: FontWeight.bold,
+                                            fontSize: 23,
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                  )
+                                ],
+                              ),
+                              const SizedBox(
+                                height: 30,
+                              ),
+                              if (isSignIn) buildSignupSection(),
+                              Column(
+                                children: [
+                                  buildInputField(
+                                      "Your Name", false, false, false),
+                                  Padding(
+                                    padding: const EdgeInsets.only(top: 10),
+                                    child: buildInputField(
+                                        "Your email", true, false, false),
+                                  ),
+                                  Padding(
+                                    padding: const EdgeInsets.only(top: 10),
+                                    child: buildInputField("Your Phone Number ",
+                                        false, false, true),
+                                  ),
+                                  Padding(
+                                    padding: const EdgeInsets.only(top: 10),
+                                    child: buildInputField(
+                                        "Password", false, true, false),
+                                  ),
+                                  ElevatedButton(
+                                      style: ElevatedButton.styleFrom(
+                                        backgroundColor: Kolors.buttonColor,
+                                      ),
+                                      onPressed: () {},
+                                      child: const Text(
+                                        "Register",
+                                        style: TextStyle(
+                                          color: Colors.white,
+                                          fontWeight: FontWeight.bold,
+                                          fontFamily: "NotoSans",
+                                          fontSize: 15,
+                                        ),
+                                      ))
+                                ],
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
                 ),
               ),
             ],
           ),
-          Positioned(
-            child: Center(
-              child: Container(
-                height: 380,
-                padding: const EdgeInsets.all(15),
-                width: MediaQuery.of(context).size.width - 40,
-                margin: const EdgeInsets.symmetric(horizontal: 20),
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.circular(15),
-                  boxShadow: [
-                    BoxShadow(
-                      color: Colors.black.withOpacity(0.3),
-                      blurRadius: 15,
-                      spreadRadius: 5,
-                    ),
-                  ],
-                ),
-                child: Column(
-                  children: [
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceAround,
-                      children: [
-                        GestureDetector(
-                          onTap: () {
-                            setState(() {
-                              isSignIn = true;
-                            });
-                          },
-                          child: Column(
-                            children: [
-                              Text(
-                                "Sign In",
-                                style: TextStyle(
-                                  color: isSignIn
-                                      ? Kolors.buttonColor
-                                      : Kolors.textColor,
-                                  fontWeight: FontWeight.bold,
-                                  fontSize: 23,
-                                ),
-                              ),
-                            ],
-                          ),
-                        ),
-                        GestureDetector(
-                          onTap: () {
-                            setState(() {
-                              isSignIn = false;
-                            });
-                          },
-                          child: Column(
-                            children: [
-                              Text(
-                                "Sign up",
-                                style: TextStyle(
-                                  color: isSignIn
-                                      ? Kolors.textColor
-                                      : Kolors.buttonColor,
-                                  fontWeight: FontWeight.bold,
-                                  fontSize: 23,
-                                ),
-                              ),
-                            ],
-                          ),
-                        )
-                      ],
-                    ),
-                    const SizedBox(
-                      height: 30,
-                    ),
-                    buildSignupSection(),
-                  ],
-                ),
-              ),
-            ),
-          ),
-          Positioned(
-            child: Align(
-              alignment: Alignment.bottomCenter,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: [
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: const [
-                      Text(
-                        "Made in",
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontWeight: FontWeight.bold,
-                          fontSize: 15,
-                        ),
-                      ),
-                      Icon(
-                        Icons.heart_broken_outlined,
-                        color: Colors.white,
-                      ),
-                      Text(
-                        "with Science",
-                        style: TextStyle(
-                            color: Colors.white, fontWeight: FontWeight.bold),
-                      )
-                    ],
-                  ),
-                ],
-              ),
-            ),
-          )
         ],
       ),
     );
